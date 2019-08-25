d610549c7c6104e43010c560a19188fadb85d595
Supot Chainiramitkul <bingli224@gmail.com>
2019-08-25 22:37:14 +0700

    Improve UI, refactor the system, and add description

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 
 /**
  * By BingLi224
- * 01:43 THA 25/08/2019
+ * 22:28 THA 25/08/2019
  *
  * Test Mutex+Arc
+ *
+ * Each threads are the players that can change the racing position with
+ * predefined role to be either -1 or 1. The player sleeps after each action.
+ * The game stops when the game position reaches the positive-or-negative
+ * target, and all other players will stop.
  */
 
 extern crate rand;
@@ -22,81 +27,99 @@ use std:: {
 use rand::Rng;
 
 struct Race {
+	target : i8,
 	over : bool,
-	position : i8
+	position : i8,
 }
 
-//fn play ( race : &mut Race, power : i8, target : i8 ) {
-fn play ( arc : &mut Arc<Mutex<Race>>, power : i8, target : i8 ) {
+fn play ( arc : &mut Arc<Mutex<Race>>, power : i8 ) {
 	let mut rnd = rand::thread_rng ( );
-        let MAX_WIDTH = 12;
+        let x_offset = 12;
 
 	loop {
-		sleep ( Duration::from_millis ( rnd.gen_range ( 100, 1000 ) ) );
+		sleep ( Duration::from_millis ( rnd.gen_range ( 10, 1000 ) ) );
 
                 let mut race = arc.lock ( ).unwrap ( );
 
+		// check if the game is already over
 		if race.over {
 			break;
 		}
 
+		// update
+		race.position += power;
+
+		// print debug data
+                print ! ( "{};\t", race.position );
+
+		// print status
+                if race.position < 0 {
+			println ! ( "{none:<space1$}[{none:<space2$}{none:<<w$}|{none:>space3$}]",
+				none="",
+				space1=(x_offset-race.target) as usize,
+				space2=(race.target+race.position) as usize,
+				w=(-race.position) as usize,
+				space3=(race.target) as usize
+			);
+                } else {
+			print ! ( "{none:<space1$}[{none:<space2$}|",
+				none="",
+				space1=( x_offset - race.target ) as usize,
+				space2=(race.target) as usize
+			);
+			if race.position > 0 {
+				println ! ( "{none:>>w$}{none:>space$}]", none="", w=(race.position) as usize, space=(race.target-race.position) as usize );
+			} else {
+				println ! ( "{none:>space$}]", none="", space=race.target as usize );
+			}
+		}
+		stdout ( ).flush ( ).unwrap ( );
+
+		// check if end of the game
 		if power < 0 {
-                    if race.position <= target {
+                    if race.position <= -race.target {
                         race.over = true;
                         break;
                     }
-		} else if race.position >= target {
+		} else if race.position >= race.target {
                     race.over = true;
                     break;
 		}
-
-		race.position += power;
-
-                println ! ( "{mark:>w$}", w=(MAX_WIDTH + race.position) as usize, mark="*" );
-                /*
-                if race.position < 0 {
-                    println ! ( "{mark:*<w$}", mark="", w=(-race.position - 1) as usize );
-                    //println ! ( "{mark:>w$}", space_sz=(MAX_WIDTH + race.position) as usize, mark=race.position );
-                    //print ! ( "\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08{pos:>w1$}", w1=MAX_WIDTH, pos=race.position );
-                } else {
-                    println ! ( "{mark:*<w$}", mark="", w=(race.position - 1) as usize );
-                    //print ! ( "\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08{space:>w1$}{pos:>w2$}", space=" ", w1=MAX_WIDTH, w2=(race.position - 1) as usize, pos=race.position );
-                }
-                */
-                //print ! ( "\r                 \r{mark:>w$}", w=(MAX_WIDTH + race.position) as usize, mark=race.position );
-                //println ! ( "{mark:>w$}", w=(MAX_WIDTH + race.position) as usize, mark=race.position );
-		stdout ( ).flush ( ).unwrap ( );
 
 	}
 }
 
 fn main ( ) {
-	let mut game = Race { 
-		over : false,
-		position : 0
+	let game = Race { 
+		target	: 10,
+		over	: false,
+		position	: 0
 	};
 
 	let mut players = vec! [ ];
 
         let arc = Arc::new ( Mutex::new ( game ) );
 
-        {
-            let mut new_arc = arc.clone ( );
-            players.push ( 
-                    thread::spawn ( move || {
-                            play ( &mut new_arc, -1, -8 )
-                    } )
-            );
-        }
+	// create players
+	for _ in 1..5 {
+		{
+		    let mut new_arc = arc.clone ( );
+		    players.push ( 
+			    thread::spawn ( move || {
+				    play ( &mut new_arc, -1 );
+			    } )
+		    );
+		}
 
-        {
-            let mut new_arc = arc.clone ( );
-            players.push ( 
-                    thread::spawn ( move || {
-                            play ( &mut new_arc, 1, 8 );
-                    } )
-            );
-        }
+		{
+		    let mut new_arc = arc.clone ( );
+		    players.push ( 
+			    thread::spawn ( move || {
+				    play ( &mut new_arc, 1 );
+			    } )
+		    );
+		}
+	}
 
 	for p in players {
 		p.join ( ).unwrap ( );
